3d17acd70f78e6259610f0e3b4a3933874e06aea
Moises Baltazar <null@moisesb.com>
2024-01-25 09:12:36 -0500

    Add initial config

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,7 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'en-US',
+  title: 'RCIF',
+  description: 'Research Computing and Informatics Facility',
+})
