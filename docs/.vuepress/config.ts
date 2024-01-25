689e302b7513f25ecd2a923a1f085bc0e5cc7b8e
Moises Baltazar <null@moisesb.com>
2024-01-25 10:08:22 -0500

    Add NavBar links from old site

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,62 @@
-import { defineUserConfig } from 'vuepress'
+import {defaultTheme, defineUserConfig} from 'vuepress'
 
 export default defineUserConfig({
-  lang: 'en-US',
-  title: 'RCIF',
-  description: 'The Research Computing and Informatics Facility (RCIF) ' +
-      'supports human imaging researchers across all campuses of ' +
-      'Washington University in St. Louis.',
-  head: [['link', { rel: 'icon', href: '/images/logo.png' }]],
+    lang: 'en-US',
+    title: 'RCIF',
+    description: 'The Research Computing and Informatics Facility (RCIF) ' +
+        'supports human imaging researchers across all campuses of ' +
+        'Washington University in St. Louis.',
+    head: [['link', {rel: 'icon', href: '/images/logo.png'}]],
+    theme: defaultTheme({
+        navbar: [
+            {
+                text: "Account",
+                link: "/getting-started/applying-for-a-user-account.md"
+            },
+            {
+                text: "Connect",
+                link: "/getting-started/connect-to-login-nodes.md"
+            },
+            {
+                text: "FAQs for Accounting",
+                link: "/getting-started/faqs-accounting.md"
+            },
+            {
+                text: "Hardware",
+                link: "/system-info/hpc-hardware.md"
+            },
+            {
+                text: "RCIF",
+                link: "/"
+            },
+            {
+                text: "RCIF Datasets",
+                link: "/getting-started/rcif-shared-datasets.md"
+            },
+            {
+                text: "Slurm Basics",
+                link: "/getting-started/slurm-basics.md"
+            },
+            {
+                text: "Software",
+                children: [
+                    {
+                        text: 'See Full List',
+                        link: '/software/software.md'
+                    },
+                    '/software/apptainer.md',
+                    "/software/jupyter-notebook.md"
+                ],
+            },
+            {
+                text: "Storage Systems",
+                link: "/getting-started/storage-systems.md"
+            },
+            {
+                text: "Training and Support",
+                link: "/getting-started/training-and-support.md"
+            }
+        ]
+    })
+
 })
